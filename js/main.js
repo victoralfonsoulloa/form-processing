fdf48b08e204f8d9049414ddcb2beb61decaad8a
Victor Ulloa <victoralfonsoulloa@gmail.com>
2024-12-04 22:44:51 -0800

    Add Title Case For Loop

--- js/main.js
+++ js/main.js
@@ -16,3 +16,13 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+// Title case for loop START
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+  }
+// Title case for loop END
